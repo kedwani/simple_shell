970d453d9fceff4a06f51550c4058b761d3df072
mo1699 <mo1699@gmail.com>
2023-11-15 20:35:39 +0200

    h

--- main.h
+++ main.h
@@ -14,9 +14,6 @@
 
 
 
-void _strtok(char *buf, char *argv[]);
-
-
 
 
 
--- test/get_max_ppid.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-max_pid=$(cat /proc/sys/kernel/pid_max)
-
-echo "The maximum PID value on this system is: $max_pid"
