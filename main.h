c604a4a59adaee3f1457442015ec2d6e7601dd4c
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-15 11:54:38 +0200

    auto push

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+char* cmd(void);
+
+
+
+
+
+
+#endif
