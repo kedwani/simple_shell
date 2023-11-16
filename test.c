7f5291f88c2faed431123d365337dbc20adcbaae
mo1699 <mo1699@gmail.com>
2023-11-16 09:32:28 +0200

    momo

--- test.c
+++ test.c
@@ -3,28 +3,28 @@ extern char **environ;
 
 int main(int argc, char *argv[],char *envp[])
 {
-    pid_t child;
-    char *buf = NULL;
-    size_t n = 0;
-    int status, i = 0;
-    char *argu[9];
-    (void) argc;
-    (void) envp;
+		pid_t child;
+		char *buf = NULL;
+		size_t n = 0;
+		int status, i = 0;
+		char *argu[9];
+		(void) argc;
+		(void) envp;
 
-    while (1)
-    {
-	    if (isatty (STDIN_FILENO))
-	    {
-		    printf("$ ");
+	while (1)
+	{
+	if (isatty (STDIN_FILENO))
+	{
+		printf("$ ");
+			if (getline(&buf, &n, stdin) == -1)
+			 {
+				perror("getline fail");
+				exit(0);
+			}
+		}
+	else
 		    if (getline(&buf, &n, stdin) == -1)
-		    {
-			    perror("getline fail");
-			    exit(EXIT_FAILURE);
-		    }
-	    }
-	    else
-		    if (getline(&buf, &n, stdin) == -1)
-			    exit(EXIT_FAILURE);
+			    exit(0);
 
 	    /* remove the enter space>>\n*/
 	    for (i = 0;buf[i] != '\0';++i)
@@ -34,7 +34,7 @@ int main(int argc, char *argv[],char *envp[])
 	    _strtok(buf, argv);
 	    if (argv[1] == NULL)
 		    continue;
-	    if (_strcmp("exit",argv[1]) == 0)
+	    if (_strcmp("exit", argv[1]) == 0)
 	    {
 		    free(buf);
 		    exit(0);
@@ -45,25 +45,25 @@ int main(int argc, char *argv[],char *envp[])
 	    {
 		    perror("fork failed");
 		    free(buf);
-		    exit(EXIT_FAILURE);
+		    exit(0);
 	    }
 	    else if (child == 0)
 	    {
-		    for (i=0;argv[i+1]!= NULL;++i)
-			    argu[i] = argv[i+1];
+		    for (i = 0;argv[ i + 1] != NULL; ++i)
+			    argu[i] = argv[i + 1];
 
 		    argu[i] = NULL;
 		    if (execve(argv[1], argu, environ) == -1)
 		    {
-			    printf("%s: No such file or directory\n",argv[0]);
+			    printf("%s: No such file or directory\n", argv[0]);
 			    free(buf);
-			    exit(EXIT_FAILURE);
+			    exit(0);
 		    }
 	    }
 	    else
 		    wait(&status);
     }
-    free (argu);
-    free(buf);
-    return 0;
+	free (argu);
+	free(buf);
+    		return	(0);
 }
