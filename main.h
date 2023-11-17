87fd481ff69019711671187859246afed75852a0
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-18 00:18:40 +0200

    momo

--- main.h
+++ main.h
@@ -11,12 +11,15 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <ctype.h>
+#include <sys/stat.h>
 
 void interactive(char **buf, size_t *n);
 void _strtok(char *buf, char *argv[]);
 char *_strstr(char *mainStr, char *sub);
 int _strcmp(char *s1, char *s2);
 void printenv (void);
+void _strttok(char *tmp,char **path);
+int path(char *buf,char **argu,char **argv,pid_t child );
 
 extern char **environ;
 
--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
-	pid_t child;
+	pid_t child = 0;
 	char *buf = NULL;
 	size_t n = 0;
 	int status = 0, i = 0;
@@ -38,7 +38,8 @@ int main(int argc, char *argv[], char *envp[])
 			printenv();
 			continue;
 		}
-		child = fork();
+		status = path(buf,argu,argv,child);
+/*		child = fork();
 		if (child == -1)
 		{
 			perror("fork failed");
@@ -56,8 +57,78 @@ int main(int argc, char *argv[], char *envp[])
 		}
 		else
 			wait(&status);
+*/
 	}
 	free(argu);
 	free(buf);
 	return	(0);
+
+}
+
+
+int path(char *buf,char **argu,char **argv,pid_t child)
+{
+        int i,status = 0;
+        char **env = environ;
+        char *tmp=NULL;
+        char * path [1024];
+	struct stat statbuf;
+        char *t;
+
+	for (i = 0;env[i] != NULL;++i)
+	{
+                if (strncmp("PATH=",env[i],5) == 0)
+                {
+                        tmp = strchr(env[i],'/');
+                        break;
+                }
+        }
+
+        strcat(tmp,":.");
+        _strttok(tmp,path);
+
+        for (i = 0;path[i] != NULL;++i)
+        {
+                t = malloc(1024);
+                strcpy(t,path[i]);
+                strcat(t,"/");
+                strcat(t,argu[0]);
+
+                if (stat(t,&statbuf) == 0)
+                {
+			child = fork();
+			if (child == -1)
+			{
+				perror("fork failed");
+				free(buf);
+				exit(0);
+			}
+			else if (child == 0)
+			{
+				strcpy(argu[0],t);
+				if (execve(argu[0], argu, environ) == -1)
+				{
+					printf("%s: No such file or directory\n", argv[0]);
+					free(buf);
+					exit(STDERR_FILENO);
+				}
+			}
+			else
+				wait(&status);
+			break;
+		}
+        }
+
+	free(t);
+	return(status);
+}
+void _strttok(char *tmp,char **path)
+{
+        int i= 1;
+        char *t = malloc(1024);
+        strcpy(t, tmp);
+        path[0] = strtok(t,":");
+	while ((path[i] = strtok(NULL,":")))
+                i++;
+	free(t);
 }
