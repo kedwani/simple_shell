62f75f2e29df0b4e862ff37f05c1973ffd419712
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-19 19:40:03 +0200

    momo

--- test.c
+++ test.c
@@ -36,53 +36,54 @@ int srch(void)
 				tmp = strdup(strchr(env[i], '/'));
 /*if PATH's value is empty or NULL (exit with 127) */
 			else
-				var.status=127*256;
+				var.status = (127 * 256);
 			break;
 		}
 	}
 /*if env's value is empty or NULL (exit with 127) */
-	if (env == NULL || strcmp(env[0],"SHLVL=0") == 0 )
-		var.status=127*256;
+	if (env == NULL || strcmp(env[0], "SHLVL=0") == 0)
+		var.status = (127 * 256);
 /*if command name is not begine with root dir or . (which indecate that
  *the excutable in the working dir)(exit with 127) */
 	if (var.command[0][0] == '/' || var.command[0][0] == '.')
 		var.status = 0;
 	else
-		var.status=127*256;
+		var.status = (127 * 256);
 /* tokenise the PATH  and excute command if found*/
 	if (tmp != NULL && tmp != "")
 	{
-		strcpy (tt,tmp);
-		i= 1;
-		path[0] = strtok(tt,":");
-		while ((path[i] = strtok(NULL,":")))
+		strcpy(tt, tmp);
+		i = 1;
+		path[0] = strtok(tt, ":");
+		while ((path[i] = strtok(NULL, ":")))
 			i++;
 		free(tmp);
-		for (i = 0;path[i] != NULL;++i)
+		for (i = 0; path[i] != NULL; ++i)
 		{
-			strcpy(t,path[i]);
-			strcat(t,"/");
-			strcat(t,var.command[0]);
-			if (stat(t,&statbuf) == 0)
+			strcpy(t, path[i]);
+			strcat(t, "/");
+			strcat(t, var.command[0]);
+			if (stat(t, &statbuf) == 0)
 			{
-				_excute(t,var.command);
-				return(0);
+				_excute(t, var.command);
+				return (0);
 			}
 		}
 	}
 	if (var.status == 256 * 127)
 	{
-		dprintf(STDERR_FILENO,"%s: 1: %s: not found\n",var.argv[0],var.command[0]);
-		return(0);
+		dprintf(STDERR_FILENO, "%s: 1: %s: not found\n", var.argv[0], var.command[0]);
+		return (0);
 	}
-	if (stat(var.command[0],&statbuf) == 0 && (var.command[0][0] == '/' || var.command[0][0] == '.'))
+	if (stat(var.command[0], &statbuf) == 0 && \
+	    (var.command[0][0] == '/' || var.command[0][0] == '.'))
 	{
-		_excute(var.command[0],var.command);
+		_excute(var.command[0], var.command);
 		return (0);
 	}
 	var.status = 512;
-	printf("%s: No such file or directory\n",var.argv[0]);
-	return(0);
+	printf("%s: No such file or directory\n", var.argv[0]);
+	return (0);
 }
 
 int rd_line (void)
@@ -93,31 +94,25 @@ int rd_line (void)
         {
 		free(var.buf);
 		perror("getline failed");
-		exit(0);
+		exit (0);
         }
 	tokenizer();
-	return(0);
+	return (0);
 }
 int tokenizer(void)
 {
-        int j = 1,i;
+        int j = 1, i;
+/*to remove the enter key  from input*/
 	for (i = 0; var.buf[i] != '\0'; ++i)
 		if (var.buf[i] == '\n')
 			var.buf[i] = '\0';
-
-/*	printf("%s\n",var.buf);*/
-
+/*tokenize the input */
         var.command[0] = strtok(var.buf, " ");
         while ((var.command[j] = strtok(NULL, " "))!= NULL)
                 ++j;
-/*	for (i=0;var.command[i] != NULL;++i)
-		printf("%s\n",var.command[i]);
-*/
-/*	free(var.buf);*/
 	if (var.command[0] == NULL)
-	{
-		return(0);
-	}
+		return (0);
+/*built in commands*/
 	if (strcmp("exit", var.command[0]) == 0)
 	{
 		free(var.buf);
@@ -126,10 +121,10 @@ int tokenizer(void)
 	if (strcmp("env", var.command[0]) == 0)
 	{
 		printenv();
-		return(0);
+		return (0);
 	}
 	srch();
-	return(0);
+	return (0);
 }
 
 int g_line (void)
@@ -148,21 +143,14 @@ int _excute (char *name, char **command)
 	var.child = fork();
 	if (var.child == 0)
 	{
-		/*	write(STDOUT_FILENO,name,strlen(name));
-		write(STDOUT_FILENO,command[0],strlen(name));
-		write(STDOUT_FILENO,command[1],strlen(name));
-		write(STDOUT_FILENO,command[2],strlen(name));
-		*/
-
 		if (execve(name, command, environ) == -1);
 		{
-			printf("%s: No such file or directory\n",var.argv[0]);
+			printf("%s: No such file or directory\n", var.argv[0]);
 			free(var.buf);
 			exit(2);
 		}
 	}
 	else
 		wait(&var.status);
-/*	free(var.argv);*/
-	return(0);
+	return (0);
 }
