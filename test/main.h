105bf7b9955e34cc6c527b5fc8eb421e256be994
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-19 21:16:49 +0200

    momo

---
+++ test/main.h
@@ -0,0 +1,46 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
+
+
+int rd_line(void);
+int tokenizer(void);
+void printenv(void);
+int srch(void);
+int g_line(void);
+int _excute(char*, char**);
+int path_tokenizer(char **tmp, char ***path);
+
+
+extern char **environ;
+
+struct info
+{
+	pid_t child;
+	char *command[100];
+	int status;
+	char *buf;
+	size_t n;
+	char **argv;
+};
+
+struct info var;
+
+
+
+
+
+#endif
---
+++ test/test.c
@@ -0,0 +1,138 @@
+#include "main.h"
+int main(int argc, char *argv[], char *envp[])
+{
+	var.status = 0;
+	(void) argc;
+	(void) envp;
+	var.argv = argv;
+
+	while (1)
+	{
+/* check is input is terminal or not */
+		if (isatty(STDIN_FILENO))
+			rd_line();
+		else
+			g_line();
+	}
+	return (var.status);
+}
+
+int srch(void)
+{
+	int i;
+	char **env = environ;
+	char *tmp = NULL;
+	char *path[100] = {0};
+	struct stat statbuf;
+	char t[1022];
+	char tt[1024];
+	for (i = 0; env[i] != NULL; ++i)
+	{
+		if (strncmp("PATH=", env[i], 5) == 0)
+		{
+			if (strchr(env[i], '/'))
+				tmp = strdup(strchr(env[i], '/'));
+			else
+				var.status = (127 * 256);
+			break;
+		}
+	}
+	if (env == NULL || strcmp(env[0], "SHLVL=0") == 0)
+		var.status = (127 * 256);
+	if (var.command[0][0] == '/' || var.command[0][0] == '.')
+		var.status = 0;
+	else
+		var.status = (127 * 256);
+	if (tmp != NULL)
+	{
+		strcpy(tt, tmp);
+		i = 1;
+		path[0] = strtok(tt, ":");
+		while ((path[i] = strtok(NULL, ":")))
+			i++;
+		free(tmp);
+		for (i = 0; path[i] != NULL; ++i)
+		{
+			strcpy(t, path[i]);
+			strcat(t, "/");
+			strcat(t, var.command[0]);
+			if (stat(t, &statbuf) == 0)
+			{
+				_excute(t, var.command);
+				return (0);
+			}
+		}
+	}
+	if (var.status == 256 * 127)
+	{
+		dprintf(STDERR_FILENO, "%s: 1: %s: not found\n", var.argv[0], var.command[0]);
+		return (0);
+	}
+	if (stat(var.command[0], &statbuf) == 0 && \
+	    (var.command[0][0] == '/' || var.command[0][0] == '.'))
+	{
+		_excute(var.command[0], var.command);
+		return (0);
+	}
+	var.status = 512;
+	printf("%s: No such file or directory\n", var.argv[0]);
+	return (0);
+}
+
+int rd_line(void)
+{
+	printf("$ ");
+
+	if (getline(&var.buf, &var.n, stdin) == -1)
+	{
+		free(var.buf);
+		perror("getline failed");
+		exit(0);
+	}
+	tokenizer();
+	return (0);
+}
+int tokenizer(void)
+{
+	int j = 1, i;
+/*to remove the enter key  from input*/
+	for (i = 0; var.buf[i] != '\0'; ++i)
+		if (var.buf[i] == '\n')
+			var.buf[i] = '\0';
+/*tokenize the input */
+	var.command[0] = strtok(var.buf, " ");
+	while ((var.command[j] = strtok(NULL, " ")) != NULL)
+		++j;
+	if (var.command[0] == NULL)
+		return (0);
+/*built in commands*/
+	if (strcmp("exit", var.command[0]) == 0)
+	{
+		free(var.buf);
+		exit(WEXITSTATUS(var.status));
+	}
+	if (strcmp("env", var.command[0]) == 0)
+	{
+		printenv();
+		return (0);
+	}
+	srch();
+	return (0);
+}
+
+int _excute(char *name, char **command)
+{
+	var.child = fork();
+	if (var.child == 0)
+	{
+		if (execve(name, command, environ) == -1)
+		{
+			printf("%s: No such file or directory\n", var.argv[0]);
+			free(var.buf);
+			exit(2);
+		}
+	}
+	else
+		wait(&var.status);
+	return (0);
+}
