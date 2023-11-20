c17d58101ec7dd9aae2cd4c551a89629fd24c8a3
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-20 10:48:48 +0200

    momo

--- main.h
+++ main.h
@@ -27,6 +27,26 @@ int path_tokinizer(void);
 
 extern char **environ;
 
+/**
+ * struct info - Structure to hold information of shell variables.
+ *
+ * The `info` structure includes the following fields:
+ * @child: A process ID
+ * @command: An array of strings to store command and its arguments.
+ * @status: An integer to store the status of the last executed command.
+ * @buf: A string to store the input line read from the terminal.
+ * @n: A size_t variable representing the
+ * size of the allocated memory for `buf`.
+ * @argv: A pointer to an array of strings
+ * representing the command-line arguments.
+ * @tmp: A pointer to a character array used for temporary storage.
+ * @path: An array of strings to store
+ * different directories in the PATH environment variable.
+ * @statbuf: A structure to store file information.
+ * @t: A character array used for temporary storage.
+ * @tt: A character array used for temporary storage.
+ *
+ */
 struct info
 {
 	pid_t child;
@@ -36,13 +56,10 @@ struct info
 	size_t n;
 	char **argv;
 	char *tmp;
-        char *path[100];
-        struct stat statbuf;
-        char t[1022];
-        char tt[1024];
-	int f;
-
-
+	char *path[100];
+	struct stat statbuf;
+	char t[1022];
+	char tt[1024];
 };
 
 struct info var;
